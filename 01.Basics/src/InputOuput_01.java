67e4cdbc684efc5746c8cf26b20cd64c01d1b171
jainkavya <jkavya865@gmail.com>
2025-10-03 18:53:00 +0530

    - Coomit -1

---
+++ 01.Basics/src/InputOuput_01.java
@@ -0,0 +1,8 @@
+public class InputOuput_01 {
+    public static void main (String[] args) {
+
+        System.out.println("Hello, World!");          // With Newline
+        System.out.print("Hello ");                  // Without Newline
+        System.out.printf("Number: %d" , 10);       // formatted
+    }
+}

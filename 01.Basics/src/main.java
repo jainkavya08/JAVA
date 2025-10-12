cd1fd744fd4ef443bfa89945a2211d91292b9c13
jainkavya <jkavya865@gmail.com>
2025-10-12 23:41:41 +0530

    Commit - practice

--- 01.Basics/src/main.java
+++ 01.Basics/src/main.java
@@ -1,10 +1,20 @@
-// Output in java
+// Taking input in  java
+
+import java.util.Scanner;
 
 public class main {
     public static void main (String[] args) {
 
-        System.out.println("Hello World");      // With newline
-        System.out.println("Kavya jain");       // without newline
-        System.out.printf("Numbers %d : " , 10);    // Formatted
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number a: ");
+        int a = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("a = " + a);
+        System.out.println("My name is : " + name);
     }
 }

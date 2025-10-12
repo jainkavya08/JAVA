9cc8b779c50e38125063c9542a5ffc145bb1c15a
jainkavya <jkavya865@gmail.com>
2025-10-12 23:25:15 +0530

    Commit - pratice

--- 01.Basics/src/main.java
+++ 01.Basics/src/main.java
@@ -1,28 +1,10 @@
-
-import java.util.Scanner;
+// Output in java
 
 public class main {
     public static void main (String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter your marks: ");
-        int marks = sc.nextInt();
-
-        if (marks >= 90 ) {
-            System.out.println("Grade A");
-        }
-        else if ( marks >= 80 ) {
-            System.out.println("Grade B");
-        }
-        else if ( marks >= 60 ) {
-            System.out.println("Grade C");
-        }
-        else if ( marks >= 50 ) {
-            System.out.println("Grade D");
-        }
-        else {
-            System.out.println("Fail");
-        }
+        System.out.println("Hello World");      // With newline
+        System.out.println("Kavya jain");       // without newline
+        System.out.printf("Numbers %d : " , 10);    // Formatted
     }
 }
